2a74503da7c32237a4f53ee481876616c58eb07d
Samar Ghourag <samarghourab@gmail.com>
2022-04-06 12:36:13 -0400

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Summer Crow on 2022-04-06.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
